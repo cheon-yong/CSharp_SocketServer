943ec535c3e835e67381608293137bb105a1b7b0
cheonyongjun <yes1312@naver.com>
2024-04-10 15:10:39 +0900

    Session #2

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -11,6 +11,13 @@ namespace ServerCore
 	{
 		Socket _socket;
 		int _disconnected = 0;
+		bool _pending = false;
+
+
+		object _lock = new object();
+		Queue<byte[]> _sendQueue = new Queue<byte[]>();
+		SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+		SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
 		// IP
 		// Recv Buffer
 		// Account
@@ -21,37 +28,66 @@ namespace ServerCore
 			_socket = socket;
 
 			// -----------------------
-			SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-			recvArgs.Completed += OnRecvCompleted;
-			recvArgs.UserToken = this;
-			recvArgs.SetBuffer(new byte[1024], 0, 1024);
+			_recvArgs.Completed += OnRecvCompleted;
+			_recvArgs.UserToken = this;
+			_recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
-			RegisterRecv(recvArgs);
+			_sendArgs.Completed += OnSendCompleted;
+
+			RegisterRecv();
 		}
 
 		public void Send(byte[] sendBuff)
 		{
-			_socket.Send(sendBuff);
+			lock (_lock)
+			{
+				_sendQueue.Enqueue(sendBuff);
+
+				if (_pending == false)
+					RegisterSend();
+			}
 		}
 
-
-		public void Disconnect()
-		{
-			if (Interlocked.Exchange(ref _disconnected, 1) == 1)
-				return;
-
-			_socket.Shutdown(SocketShutdown.Both);
-			_socket.Close();
-			_socket.Dispose();
-		}
 
 		#region 네트워크 통신
-		void RegisterRecv(SocketAsyncEventArgs args)
+		void RegisterSend()
 		{
-			bool pending = _socket.ReceiveAsync(args);
+			_pending = true;
+
+			byte[] buff = _sendQueue.Dequeue();
+			_sendArgs.SetBuffer(buff, 0, buff.Length);
+
+			bool pending = _socket.SendAsync(_sendArgs);
+			if (pending)
+				OnSendCompleted(null, _sendArgs);
+		}
+
+		void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+		{
+            lock (_lock)
+            {
+				if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+				{
+					if (_sendQueue.Count > 0)
+						RegisterSend();
+					else
+						_pending = false;
+				}
+				else
+				{
+					Disconnect();
+				}
+			}
+            
+		}
+
+
+		void RegisterRecv()
+		{
+			bool pending = _socket.ReceiveAsync(_recvArgs);
             if (pending == false)
             {
-				OnRecvCompleted(null, args);
+				OnRecvCompleted(null, _recvArgs);
 			}
         }
 
@@ -62,12 +98,21 @@ namespace ServerCore
 				// TODO
 				string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                 Console.WriteLine($"[From Client] {recvData}");
-				RegisterRecv(args);
+				RegisterRecv();
             }
 			else
 			{
 				// TODO Disconnect
 			}
+		}
+		public void Disconnect()
+		{
+			if (Interlocked.Exchange(ref _disconnected, 1) == 1)
+				return;
+
+			_socket.Shutdown(SocketShutdown.Both);
+			_socket.Close();
+			_socket.Dispose();
 		}
 		#endregion
 	}
